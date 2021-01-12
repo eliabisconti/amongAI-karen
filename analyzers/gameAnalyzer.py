db4503d161ea88c7db8333fa83c7749c80b338ef
Matteo <matteo.montagnani8@gmail.com>
2021-01-12 19:46:17 +0100

    Inserita predizione

--- analyzers/gameAnalyzer.py
+++ analyzers/gameAnalyzer.py
@@ -1,6 +1,10 @@
 import time
+import pickle
 from threading import Thread
+from numpy import array
+import tensorflow as tf
 
+from keras.utils import to_categorical
 from scipy.spatial import distance
 
 from data_structure import gameStatus
@@ -36,6 +40,9 @@ class gameAnalyzer(Thread):
                                                                                      gameStatus.game.wantedFlagY])
 
     def run(self):
+
+        a_file = open("lstm_prediction\LSTM_model2.pickle", "rb")
+        model = pickle.load(a_file)
 
         while gameStatus.game.state == "ACTIVE":
 
@@ -92,6 +99,70 @@ class gameAnalyzer(Thread):
 
             gameStatus.game.nearestRecharge = nearestRecharge
             gameStatus.game.nearestEnemyLinearDistance = nearestEnemy
+
+            if gameStatus.game.stage is not 0:
+                for k in gameStatus.game.enemies.keys():
+                    #print('CICLOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO')
+                    if len(gameStatus.game.enemies.get(k).actionList) > 0:
+                        print('CHIAVE ' + str(k) + ' ' + 'lista: ' + str(gameStatus.game.enemies.get(k).actionList))
+
+                        tmp_act = []
+                        tmp_pred = []
+                        gameStatus.game.enemies.get(k).predictedActions = []
+                        for a in gameStatus.game.enemies.get(k).actionList:
+                            tmp_act.append(a)
+                        # ottengo una serie di predizioni lunga 10
+                        #print('ACTTTTT ' + str(tmp_act))
+
+                        for i in range(1):
+                            # preparo array di input al modello prendendo le azioni fatte finora
+                            x_input = array(tmp_act)
+                            x_input = to_categorical(x_input, num_classes=3)
+                            x_input = x_input.reshape((1, len(x_input), 3))
+                            # predizione e estrazione dell'azione
+                            #with graph.as_default():
+                            pred = model.predict(x_input, verbose=0)
+                            max_pred = 0
+                            max_pos = 0
+                            for z in range(3):
+                                if pred[0][z] > max_pred:
+                                    max_pred = pred[0][z]
+                                    max_pos = z
+                            if max_pos == 0:
+                                pred_dec = 0
+                            elif max_pos == 1:
+                                pred_dec = 1
+                            else:
+                                pred_dec = 2
+                            tmp_pred.append(pred_dec)
+                            tmp_act.append(pred_dec)
+                        # aggiornamento array azioni predette in GameStatus
+                        for j in tmp_pred:
+                            gameStatus.game.enemies.get(k).predictedActions.append(j)
+
+                        count_0 = 0
+                        count_1 = 0
+                        count_2 = 0
+                        max_count = 0
+                        max_i = None
+                        for x in tmp_act:
+                            if x == 0:
+                                count_0 += 1
+                                if count_0 > max_count:
+                                    max_count = count_0
+                                    max_i = 0
+                            elif x == 1:
+                                count_1 += 1
+                                if count_1 > max_count:
+                                    max_count = count_1
+                                    max_i = 1
+                            elif x == 2:
+                                count_2 += 1
+                                if count_2 > max_count:
+                                    max_count = count_2
+                                    max_i = 2
+                        gameStatus.game.enemies.get(k).classificatedAs = max_i
+                        print('CLASSE: ' +  str(gameStatus.game.enemies.get(k).classificatedAs))
 
 
             """
